63f0e9b6f247fccd6b2f20ef1570fd514d8ce053
buitruong1234 <truongbqph26905@fpt.edu.vn>
2024-07-16 08:47:27 +0700

    dmmmmm

--- da1/admin/category/update.php
+++ da1/admin/category/update.php
@@ -19,7 +19,7 @@
             <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="index.php" class="d-block">BDT Fashion</a>
+            <a href="index.php" class="d-block">BDT Fashionnnnnn</a>
           </div>
         </div>
 
